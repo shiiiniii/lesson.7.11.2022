b3030c6fb2552203c1a5546ffce932d07e425c39
Anastasia <nastya-shishkina-1999@mail.ru>
2022-11-07 13:25:13 +0300

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 30: Напишите программу, которая
+//выводит массив из 8 элементов, заполненный
+//нулями и единицами в случайном порядке.
+//[1,0,1,1,0,1,0,0]
+
+
+Console.Clear();
+Console.WriteLine("Введите количество элементов массива : ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] NameArray(int size)
+{
+    int[] array = new int[size]; //
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(0, 2);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{   
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i>array.Length-1) Console.Write(",");
+    }
+    Console.Write("]");
+}
+
+int[] nameArray = NameArray(num);
+PrintArray(nameArray);
