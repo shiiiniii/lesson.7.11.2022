992ef8c497b64df3a2f3ef531a5ded18d31dbff5
Anastasia <nastya-shishkina-1999@mail.ru>
2022-11-16 20:37:21 +0300

    task 44

---
+++ task44/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 44: Не используя рекурсию, выведите первые 
+//N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+//Если N = 5 -> 0 1 1 2 3
+//Если N = 3 -> 0 1 1
+//Если N = 7 -> 0 1 1 2 3 5 8
+
+Console.Clear();
+Console.WriteLine(" Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] Fibonacci(int num)
+{
+   
+    int[] arrayFibonacci = new int [num];
+    arrayFibonacci[1] = 1;
+    for (int i = 2; i < num; i++)
+    {
+        arrayFibonacci[i] = arrayFibonacci[i - 1] + arrayFibonacci[i - 2];
+    }
+    return arrayFibonacci;
+}
+
+
+void PrintArray (int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int[] fibonacci = Fibonacci(number);
+PrintArray(fibonacci);
