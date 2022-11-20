239632362c73f654b1e98a6d57332c8a350006d7
Anastasia <nastya-shishkina-1999@mail.ru>
2022-11-20 15:44:11 +0300

    task 51

---
+++ task51/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 51: Задайте двумерный массив. 
+//Найдите сумму элементов, находящихся на 
+//главной диагонали (с индексами
+//(0,0); (1;1) и т.д.
+
+
+Console.Clear();
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    
+    for (int j = 0; j < matrix.GetLength(1); j++) 
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++) 
+        {
+            matrix[i, j] = rnd.Next(min, max + 1); 
+        }
+    }
+    return matrix;
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i,j], 5} |");
+            else Console.Write($"{matrix[i, j], 5}");
+            }
+            Console.WriteLine(" |");
+            }
+}
+
+int SumMatrixCrossLine(int[,] matrix)
+{
+    int sum = default;
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+    {
+        
+        for (int j = 0; j < matrix.GetLength(0); j++) 
+        {
+            if(i == j)
+            {
+                sum = matrix[i, j] + sum;
+            }
+        }  
+    }
+    return sum;
+}
+
+
+int[,] array2D = CreateMatrixRndInt(3, 6, -10, 10);
+PrintMatrix(array2D);
+Console.WriteLine();
+int sumMatrixCrossLine = SumMatrixCrossLine(array2D);
+Console.WriteLine(sumMatrixCrossLine);
