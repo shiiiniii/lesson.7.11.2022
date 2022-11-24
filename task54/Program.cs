22ed41bf47f7866c7f2e3444bb766fbf603fb16d
Anastasia <nastya-shishkina-1999@mail.ru>
2022-11-24 23:08:16 +0300

    task 54

---
+++ task54/Program.cs
@@ -0,0 +1,86 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//В итоге получается вот такой массив:
+//7 4 2 1
+//9 5 3 2
+//8 4 4 2
+
+
+Console.Clear();
+Console.Write("Введите количество строк двумерного массива : ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов двумерного массива : ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5}");
+            if (j < matrix.GetLength(1) - 1) Console.Write(" |");
+        }
+    Console.WriteLine(" ]");
+    }
+}
+
+
+void OrderArrayLines(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+
+void PrintArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i, j] + " | ");
+    }
+    Console.WriteLine();
+  }
+}
+
+
+int[,] newMatrixRndInt = CreateMatrixRndInt(m, n, 1, 10);
+Console.WriteLine();
+PrintMatrix(newMatrixRndInt);
+Console.WriteLine("Отсортированный массив: ");
+OrderArrayLines(newMatrixRndInt);
+PrintArray(newMatrixRndInt);
