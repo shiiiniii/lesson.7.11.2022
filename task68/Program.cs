699038e2e2972e5e1aa637be7e0adb993ab66173
Anastasia <nastya-shishkina-1999@mail.ru>
2022-11-26 21:10:25 +0300

    task 69

---
+++ task68/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+//Даны два неотрицательных числа m и n.
+//m = 2, n = 3 -> A(m,n) = 9
+//m = 3, n = 2 -> A(m,n) = 29
+
+Console.Clear();
+Console.Write("Программа вычисления функции Аккермана");
+Console.WriteLine();
+Console.Write("Введите первое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int AkkermanFunction(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    if (n == 0 && m > 0)
+    {
+        return AkkermanFunction(m - 1, 1);
+    }
+    else
+    {
+        return AkkermanFunction(m - 1, AkkermanFunction(m, n - 1));
+    }
+}
+
+while(m < 0) 
+{
+    Console.WriteLine($"Вы ввели первое число отрицательным.");
+    Console.WriteLine($"Введите натуральное число: ");
+    m = Convert.ToInt32(Console.ReadLine());
+}
+
+while(n < 0) 
+{
+    Console.WriteLine($"Вы ввели второе число отрицательным.");
+    Console.WriteLine($"Введите натуральное число: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+
+int akkermanFunction = AkkermanFunction(m, n);
+Console.Write($"m = {m}, n = {n} - > A(m,n) = {akkermanFunction} ");
---
+++ task69/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 69: Напишите программу, которая на вход 
+//принимает два числа A и B, и возводит число 
+//А в целую степень B с помощью рекурсии.
+//A = 3; B = 5 -> 243 (3⁵)
+//A = 2; B = 3 -> 8
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень в которую будете возводить натуральное число: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+int Exponation(int numA, int numB)
+{
+    if(numB == 0) return 1;
+    return numA * Exponation(numA, numB - 1);
+}
+while(numB < 0) 
+{
+    Console.WriteLine($"Введено отрицательное число.");
+    Console.WriteLine($"Введите натуральное число степени: ");
+    numB = Convert.ToInt32(Console.ReadLine());
+}
+
+    int exponation = Exponation(numA, numB);
+    Console.WriteLine($"Число {numA} в {numB} степени = {exponation}");
+
