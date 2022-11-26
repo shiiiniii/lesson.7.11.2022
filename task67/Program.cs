8fc3478ec4d35ea427914d0ac74e3efd0ed70659
Anastasia <nastya-shishkina-1999@mail.ru>
2022-11-26 21:10:03 +0300

    task 67

---
+++ task67/Program.cs
@@ -0,0 +1,19 @@
+﻿//семинар//Задача 67: Напишите программу, которая будет 
+//принимать на вход число и возвращать сумму его цифр.
+//453 -> 12
+//45 -> 9
+
+Console.Clear();
+Console.Write("Введите многозначное натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumNumbers(int num)
+{
+   if(num == 0) return num;
+   return num % 10 + SumNumbers(num / 10);   
+}
+
+int sumNumbers = SumNumbers(number);
+Console.Write($"Сумма цифр вашего числа = {sumNumbers}");
+
+
